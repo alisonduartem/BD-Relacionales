a1e01fe5bbe901a26a7418dbdb3b213d705f8948
Alis DM <alison.duarte24@cetis107.edu.mx>
2026-02-25 08:13:17 -0700

    Add files via upload

---
+++ spotify r.sql
@@ -0,0 +1,316 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-02-2026 a las 16:12:32
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `spotify`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `artistas`
+--
+
+CREATE TABLE `artistas` (
+  `id artistas` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `artista_canciones`
+--
+
+CREATE TABLE `artista_canciones` (
+  `id artistas` int(11) NOT NULL,
+  `id_cancion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `canciones`
+--
+
+CREATE TABLE `canciones` (
+  `id_cancion` int(11) NOT NULL,
+  `nombre` varchar(11) NOT NULL,
+  `duracion` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `canciones`
+--
+
+INSERT INTO `canciones` (`id_cancion`, `nombre`, `duracion`) VALUES
+(1, 'neo roneo', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cancion_genero`
+--
+
+CREATE TABLE `cancion_genero` (
+  `id cancion` int(11) NOT NULL,
+  `id genero` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `favoritos`
+--
+
+CREATE TABLE `favoritos` (
+  `id usuario` int(11) NOT NULL,
+  `id cancion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `generos`
+--
+
+CREATE TABLE `generos` (
+  `id generos` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `membresia`
+--
+
+CREATE TABLE `membresia` (
+  `id_membresia` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL,
+  `precio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `playlist`
+--
+
+CREATE TABLE `playlist` (
+  `id playlist` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL,
+  `n_canciones` int(11) NOT NULL,
+  `duracion` int(11) NOT NULL,
+  `id usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `playlist_canciones`
+--
+
+CREATE TABLE `playlist_canciones` (
+  `id playlist` int(11) NOT NULL,
+  `id cancion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id usuario` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL,
+  `membresia_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `artistas`
+--
+ALTER TABLE `artistas`
+  ADD PRIMARY KEY (`id artistas`);
+
+--
+-- Indices de la tabla `artista_canciones`
+--
+ALTER TABLE `artista_canciones`
+  ADD KEY `id artistas` (`id artistas`),
+  ADD KEY `id canciones` (`id_cancion`);
+
+--
+-- Indices de la tabla `canciones`
+--
+ALTER TABLE `canciones`
+  ADD PRIMARY KEY (`id_cancion`);
+
+--
+-- Indices de la tabla `cancion_genero`
+--
+ALTER TABLE `cancion_genero`
+  ADD KEY `id cancion` (`id cancion`),
+  ADD KEY `id genero` (`id genero`);
+
+--
+-- Indices de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD KEY `id usuario` (`id usuario`),
+  ADD KEY `id cancion` (`id cancion`);
+
+--
+-- Indices de la tabla `generos`
+--
+ALTER TABLE `generos`
+  ADD PRIMARY KEY (`id generos`);
+
+--
+-- Indices de la tabla `membresia`
+--
+ALTER TABLE `membresia`
+  ADD PRIMARY KEY (`id_membresia`);
+
+--
+-- Indices de la tabla `playlist`
+--
+ALTER TABLE `playlist`
+  ADD PRIMARY KEY (`id playlist`),
+  ADD KEY `id usuario` (`id usuario`);
+
+--
+-- Indices de la tabla `playlist_canciones`
+--
+ALTER TABLE `playlist_canciones`
+  ADD KEY `id playlist` (`id playlist`),
+  ADD KEY `id cancion` (`id cancion`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id usuario`),
+  ADD KEY `membresia_id` (`membresia_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `artistas`
+--
+ALTER TABLE `artistas`
+  MODIFY `id artistas` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `canciones`
+--
+ALTER TABLE `canciones`
+  MODIFY `id_cancion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `generos`
+--
+ALTER TABLE `generos`
+  MODIFY `id generos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `membresia`
+--
+ALTER TABLE `membresia`
+  MODIFY `id_membresia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `playlist`
+--
+ALTER TABLE `playlist`
+  MODIFY `id playlist` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `playlist_canciones`
+--
+ALTER TABLE `playlist_canciones`
+  MODIFY `id playlist` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `artistas`
+--
+ALTER TABLE `artistas`
+  ADD CONSTRAINT `artistas_ibfk_1` FOREIGN KEY (`id artistas`) REFERENCES `artista_canciones` (`id artistas`);
+
+--
+-- Filtros para la tabla `artista_canciones`
+--
+ALTER TABLE `artista_canciones`
+  ADD CONSTRAINT `artista_canciones_ibfk_1` FOREIGN KEY (`id artistas`) REFERENCES `artistas` (`id artistas`),
+  ADD CONSTRAINT `artista_canciones_ibfk_2` FOREIGN KEY (`id_cancion`) REFERENCES `canciones` (`id_cancion`);
+
+--
+-- Filtros para la tabla `cancion_genero`
+--
+ALTER TABLE `cancion_genero`
+  ADD CONSTRAINT `cancion_genero_ibfk_1` FOREIGN KEY (`id cancion`) REFERENCES `canciones` (`id_cancion`),
+  ADD CONSTRAINT `cancion_genero_ibfk_2` FOREIGN KEY (`id genero`) REFERENCES `generos` (`id generos`);
+
+--
+-- Filtros para la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD CONSTRAINT `favoritos_ibfk_1` FOREIGN KEY (`id cancion`) REFERENCES `canciones` (`id_cancion`),
+  ADD CONSTRAINT `favoritos_ibfk_2` FOREIGN KEY (`id usuario`) REFERENCES `usuarios` (`id usuario`);
+
+--
+-- Filtros para la tabla `playlist`
+--
+ALTER TABLE `playlist`
+  ADD CONSTRAINT `playlist_ibfk_1` FOREIGN KEY (`id usuario`) REFERENCES `usuarios` (`id usuario`);
+
+--
+-- Filtros para la tabla `playlist_canciones`
+--
+ALTER TABLE `playlist_canciones`
+  ADD CONSTRAINT `playlist_canciones_ibfk_1` FOREIGN KEY (`id playlist`) REFERENCES `playlist` (`id playlist`),
+  ADD CONSTRAINT `playlist_canciones_ibfk_2` FOREIGN KEY (`id cancion`) REFERENCES `canciones` (`id_cancion`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`membresia_id`) REFERENCES `membresia` (`id_membresia`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
